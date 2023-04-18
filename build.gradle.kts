0181da3260728453df4f1ad77d5a7740d2f838e0
slo0ey <rurchi1206@gmail.com>
2023-04-18 23:20:48 +0900

    mysql connector 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version : String by project
-val h2_version : String by project
+val mysql_version : String by project
 val hikaricp_version : String by project
 
 plugins {
@@ -31,7 +31,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
+    implementation("mysql:mysql-connector-java:$mysql_version")
     implementation("com.zaxxer:HikariCP:$hikaricp_version")
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
