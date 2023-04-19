50ccde09cc185ea76803e25c609b433c08ec92e5
slo0ey <rurchi1206@gmail.com>
2023-04-19 13:21:02 +0900

    각각의 Route 추가

--- src/main/kotlin/com/github/slo0ey/kwe/global/plugin/Routing.kt
+++ src/main/kotlin/com/github/slo0ey/kwe/global/plugin/Routing.kt
@@ -1,10 +1,15 @@
 package com.github.slo0ey.kwe.global.plugin
 
+import com.github.slo0ey.kwe.domain.building.buildingRoute
+import com.github.slo0ey.kwe.domain.city.cityRoute
+import com.github.slo0ey.kwe.domain.user.userRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.setUpRouter() {
     install(Routing) {
-        // Soon...
+        userRoute()
+        cityRoute()
+        buildingRoute()
     }
 }
